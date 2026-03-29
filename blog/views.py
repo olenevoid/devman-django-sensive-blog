67903e4cb9703429631329e428f768c1aade8270
olenevoid <mazekunov@gmail.com>
2026-03-29 18:00:24 +0500

    Use count instead of len() in tag serialization

--- blog/views.py
+++ blog/views.py
@@ -38,7 +38,7 @@ def serialize_post_optimized(post):
 def serialize_tag(tag):
     return {
         'title': tag.title,
-        'posts_with_tag': len(Post.objects.filter(tags=tag)),
+        'posts_with_tag': Post.objects.filter(tags=tag).count(),
     }
 
 
