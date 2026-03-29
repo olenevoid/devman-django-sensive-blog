a310333b3a0ffdbe60fa1bb3b6ce04e10888c9b9
olenevoid <mazekunov@gmail.com>
2026-03-29 11:54:22 +0500

    Optimize comments count

--- blog/views.py
+++ blog/views.py
@@ -8,7 +8,7 @@ def get_related_posts_count(tag):
 
 
 def get_most_popular_posts(number_of_posts = 5):
-    all_posts = Post.objects.annotate(likes_count=Count('likes'))
+    all_posts = Post.objects.annotate(likes_count=Count('likes', distinct=True), comments_count=Count('comments', distinct=True))
     return all_posts.order_by('-likes_count')[:number_of_posts]
 
 
@@ -31,6 +31,20 @@ def serialize_post(post):
     }
 
 
+def serialize_post_optimized(post):
+    return {
+        'title': post.title,
+        'teaser_text': post.text[:200],
+        'author': post.author.username,
+        'comments_amount': post.comments_count,
+        'image_url': post.image.url if post.image else None,
+        'published_at': post.published_at,
+        'slug': post.slug,
+        'tags': [serialize_tag(tag) for tag in post.tags.all()],
+        'first_tag_title': post.tags.all()[0].title,
+    }
+
+
 def serialize_tag(tag):
     return {
         'title': tag.title,
@@ -42,16 +56,16 @@ def index(request):
 
     most_popular_posts = get_most_popular_posts().prefetch_related('author')
 
-    fresh_posts = Post.objects.order_by('published_at')
+    fresh_posts = Post.objects.order_by('published_at').annotate(likes_count=Count('likes', distinct=True)).annotate(comments_count=Count('comments', distinct=True))
     most_fresh_posts = list(fresh_posts.prefetch_related('author'))[-5:]
 
     most_popular_tags = get_most_popular_tags()
 
     context = {
         'most_popular_posts': [
-            serialize_post(post) for post in most_popular_posts
+            serialize_post_optimized(post) for post in most_popular_posts
         ],
-        'page_posts': [serialize_post(post) for post in most_fresh_posts],
+        'page_posts': [serialize_post_optimized(post) for post in most_fresh_posts],
         'popular_tags': [serialize_tag(tag) for tag in most_popular_tags],
     }
     return render(request, 'index.html', context)
