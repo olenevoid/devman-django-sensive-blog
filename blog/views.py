2d4db655e18341350c454b9b5e32145b0affff2c
olenevoid <mazekunov@gmail.com>
2026-04-01 13:46:40 +0500

    Optimize likes count in post_detail

--- blog/views.py
+++ blog/views.py
@@ -52,7 +52,7 @@ def index(request):
 def post_detail(request, slug):
     post = Post.objects\
         .select_related('author')\
-        .prefetch_related('likes')\
+        .annotate(likes_count=Count('likes'))\
         .fetch_tags_with_posts_count()\
         .get(slug=slug)
     comments = post.comments.prefetch_related('author')
@@ -64,8 +64,6 @@ def post_detail(request, slug):
             'author': comment.author.username,
         })
 
-    likes = post.likes.all()
-
     related_tags = post.tags.all()
 
     serialized_post = {
@@ -73,7 +71,7 @@ def post_detail(request, slug):
         'text': post.text,
         'author': post.author.username,
         'comments': serialized_comments,
-        'likes_amount': len(likes),
+        'likes_amount': post.likes_count,
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
